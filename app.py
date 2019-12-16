023adcf8961be785f7c0713471825f1849130541
AlonsoMD <alonso2295@hotmail.com>
2019-12-15 21:04:38 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from flask_restful import Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # This turn off the flask SQLAlchemy modification tracker because SQLAlchemy has its own.
+app.secret_key = 'jose'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# https://blog.tecladocode.com/learn-python-advanced-configuration-of-flask-jwt/
+#app.config["JWT_AUTH_URL_RULE"] = '/login'
+jwt = JWT(app,authenticate,identity) # create new endpoint /auth
+#app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=3600)
+#app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+
+#@jwt.auth_response_handler
+#def customized_response_handler(access_token, identity):
+#    return jsonify({
+#                        'access_token': access_token.decode('utf-8'),
+#                        'user_id': identity.id
+#                   })
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList,'/stores')
+api.add_resource(UserRegister, '/register')
+
+from db import db
+db.init_app(app)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
---
+++ models/store.py
@@ -0,0 +1,28 @@
+from db import db
+
+class StoreModel(db.Model):
+    __tablename__ = "stores"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    items = db.relationship('ItemModel', lazy = 'dynamic')
+
+
+    def __init__(self,name):
+        self.name = name  
+
+    def json(self):
+        return {'name': self.name, 'items': list(map(lambda x: x.json(), self.items.all()))}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+    
+    def save_to_db(self): #useful for insert and update
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+    
